e3cca0684845e4d484009431c0346231d0d1f0b8
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-09-10 13:55:43 +0700

    change router

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -10,10 +10,10 @@ const AppRouter = () => {
     <RouterWrapper>
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<HomePage />} />
-        <Route path='/slots' element={<SlotPage />} />
       </Route>
       <Route path="/admin" element={<AdminLayout />}>
         <Route index element={<HomePage />} />
+        <Route path='slots' element={<SlotPage />} />
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </RouterWrapper>
